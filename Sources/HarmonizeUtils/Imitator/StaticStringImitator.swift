ebde2c7978e6549ac767b39a339d92aa4d7adb82
Lucas Cavalcante <co.lucasc@gmail.com>
2024-11-27 10:02:21 -0300

    Add new Apache 2.0 license header.

--- Sources/HarmonizeUtils/Imitator/StaticStringImitator.swift
+++ Sources/HarmonizeUtils/Imitator/StaticStringImitator.swift
@@ -1,8 +1,20 @@
 //
 //  StaticStringImitator.swift
-//  SwiftLint
+//  Harmonize
 //
-//  Copyright (c) 2020 Realm Inc. All Rights Reserved.
+//  Copyright 2024 Perry Street Software Inc.
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+
+//  http://www.apache.org/licenses/LICENSE-2.0
+
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 // Taken from: https://github.com/realm/SwiftLint/blob/9ebd6ae9d20798b111bd7088e3c247be4473b006/Tests/IntegrationTests/IntegrationTests.swift#L74
