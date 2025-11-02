0f5fa9357504b8427457e156e754ab4b95f418cc
Eric Silverberg <eric@perrystreet.com>
2025-11-02 18:39:13 -0500

    Add ability to filter arrays of extensions by the type they extend

---
+++ Tests/Fixtures/Filters/Extensions/ExtensionsFixtures.swift
@@ -0,0 +1,14 @@
+//
+//  ExtensionsFilters.swift
+//  Harmonize
+//
+//  Copyright (c) Perry Street Software 2025. All Rights Reserved.
+//
+
+import Foundation
+
+extension String {
+    var isHello: Bool {
+        return self == "hello"
+    }
+}
